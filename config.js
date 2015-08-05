348b28ecf976133e710b097fe3fa4f9f4e0d8c1b
Farrukh Mamatkhalilov <faxa007@gmail.com>
2015-08-05 21:17:05 +0500

    Configurations file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // your secret key
+  'secret' : 'ilovenodejsandangularjs',
+  // mongodb URI. I used modulus.io
+  'database': 'mongodb://farrukh:12345678@apollo.modulusmongo.net:27017/ago2quGe'
+}
